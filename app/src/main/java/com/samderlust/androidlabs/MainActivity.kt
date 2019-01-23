93b6011cf673a64effa5b648dfa9c8569567195e
Samderlust 9 <samderlust@gmail.com>
2019-01-23 08:41:05 -0500

    add Intent

--- app/src/main/java/com/samderlust/androidlabs/MainActivity.kt
+++ app/src/main/java/com/samderlust/androidlabs/MainActivity.kt
@@ -1,37 +1,44 @@
 package com.samderlust.androidlabs
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var linearLayout: Intent
-    lateinit var relativeLayout: Intent
-    lateinit var gridLayout: Intent
-    lateinit var button: Button
 
-    fun linearClick(v: View){
-        startActivity(linearLayout)
+    fun onLogin(v: View){
+        val profilePage = Intent(applicationContext, ProfileActivity::class.java)
+
+        profilePage.putExtra("email", emailInput.text.toString())
+
+        startActivity(profilePage)
+
     }
-
-    fun gridClick(v: View){
-        startActivity(gridLayout)
-    }
-
-    fun relativeClick(v: View){
-        startActivity(relativeLayout)
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.button321)
-        linearLayout = Intent(applicationContext, Linear::class.java)
-        relativeLayout = Intent(applicationContext, Relative::class.java)
-        gridLayout = Intent(applicationContext, Grid::class.java)
+        setContentView(R.layout.activity_login)
+
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        val savedEmail = sharedPref.getString(getString(R.string.emailSaving), "")
+
+        emailInput.hint = savedEmail
+
+        println("SAVE EMAIL ###########:" + savedEmail)
 
     }
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putString(getString(R.string.emailSaving),emailInput.text.toString())
+            commit()
+        }
+    }
+
 }
---
+++ app/src/main/java/com/samderlust/androidlabs/ProfileActivity.kt
@@ -0,0 +1,53 @@
+package com.samderlust.androidlabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import kotlinx.android.synthetic.main.activity_profile.*
+import android.provider.MediaStore
+import android.content.Intent
+import android.graphics.Bitmap
+
+
+
+
+
+class ProfileActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    fun onTakePicture(v: View){
+        dispatchTakePictureIntent()
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val email  = intent.getStringExtra("email")
+        println(" EMAIL #########   " +email)
+
+        profileEmailField.setText(email)
+    }
+
+    fun dispatchTakePictureIntent() {
+    val takePictureIntent =  Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+}
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val imageBitmap = extras?.get("data") as Bitmap
+            yourPicture.setImageBitmap(imageBitmap)
+        }
+    }
+
+
+
+}
